5df1a82064368498a2444e5ae88780e1fcb72627
Nielsen Pierce <nielsen.pierce@rackspace.co.uk>
2014-10-20 16:17:53 +0100

    Page version 2

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
             <div class="rs-main">
                 <div class="rs-content rs-panel">
                     <div class="rs-inner">
-                        <h2 class="rs-page-title">Demo Code Version 1</h2>
+                        <h2 class="rs-page-title">Demo Code Version 2</h2>
                         <p><?php echo $this->data['message']; ?></p>
                     </div>
                 </div>
