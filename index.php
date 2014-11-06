7c66f2b2d65c30be293028da48e642d0fa8adcc5
Nielsen Pierce <nielsen.pierce@rackspace.co.uk>
2014-11-06 14:55:39 +0000

    Version 2 logo update

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
                     <div class="rs-inner">
                         <h2 class="rs-page-title">Demo Code Version 2</h2>
                         <p><?php echo 'Commit ID: '; include 'version.php'; ?></p>
-                        <img src="http://9a235d8f41b67f7cad4b-8aac7af9128132fac7c466e9c0db0d1d.r1.cf3.rackcdn.com/logo2.png" />
+                        <img src="http://9a235d8f41b67f7cad4b-8aac7af9128132fac7c466e9c0db0d1d.r1.cf3.rackcdn.com/logo3.png" />
                     </div>
                 </div>
                 </div>
